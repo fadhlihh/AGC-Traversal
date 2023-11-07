f9deac3c4feb094e151a61ffae9ff0b4d0e0f393
Fadhli Hibatul Haqqi <fadhlimorth5@gmail.com>
2023-11-07 17:26:40 +0700

    create input manager to detect player input

---
+++ Assets/Game/Scripts/Input/InputManager.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private void Update()
+    {
+        CheckMovementInput();
+        CheckSprintInput();
+        CheckJumpInput();
+        CheckCrouchInput();
+        CheckChangePOVInput();
+        CheckClimbInput();
+        CheckGlideInput();
+        CheckCancelInput();
+    }
+
+    private void CheckMovementInput()
+    {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        Debug.Log("Vertical Axis: " + verticalAxis);
+        Debug.Log("Horizontal Axis: " + horizontalAxis);
+    }
+
+    private void CheckSprintInput()
+    {
+        bool isHoldSprintInput = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        if (isHoldSprintInput)
+        {
+            Debug.Log("Sprinting");
+        }
+        else
+        {
+            Debug.Log("Not Sprinting");
+        }
+    }
+
+    private void CheckJumpInput()
+    {
+        bool isPressJumpInput = Input.GetKeyDown(KeyCode.Space);
+        if (isPressJumpInput)
+        {
+            Debug.Log("Jump");
+        }
+    }
+
+    private void CheckCrouchInput()
+    {
+        bool isPressCrouchInput = Input.GetKeyDown(KeyCode.LeftControl) ||
+                                    Input.GetKeyDown(KeyCode.RightControl);
+        if (isPressCrouchInput)
+        {
+            Debug.Log("Crouch");
+        }
+    }
+
+    private void CheckChangePOVInput()
+    {
+        bool isPressChangePOVInput = Input.GetKeyDown(KeyCode.Q);
+        if (isPressChangePOVInput)
+        {
+            Debug.Log("Change POV");
+        }
+    }
+
+    private void CheckClimbInput()
+    {
+        bool isPressClimbInput = Input.GetKeyDown(KeyCode.E);
+        if (isPressClimbInput)
+        {
+            Debug.Log("Climb");
+        }
+    }
+
+    private void CheckGlideInput()
+    {
+        bool isPressGlideInput = Input.GetKeyDown(KeyCode.G);
+        if (isPressGlideInput)
+        {
+            Debug.Log("Glide");
+        }
+    }
+
+    private void CheckCancelInput()
+    {
+        bool isPressCancelInput = Input.GetKeyDown(KeyCode.C);
+        if (isPressCancelInput)
+        {
+            Debug.Log("Cancel Climb or Glide");
+        }
+    }
+}
